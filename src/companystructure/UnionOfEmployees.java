469fd0f36942deb49f22339513fd625f26b8c506
Godwin <godwinmail6@gmail.com>
2023-10-05 16:13:22 +0200

    created attributes

---
+++ src/companystructure/UnionOfEmployees.java
@@ -0,0 +1,17 @@
+package companystructure;
+
+public class UnionOfEmployees {
+
+        public Department production1;
+        public  Department getProduction2;
+        public  Department admin;
+        public  Department sales;
+
+        for(Department role : )
+
+
+
+
+
+
+}
